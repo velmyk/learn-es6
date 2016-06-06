8ee267bb07ff6516b96823a054a84460080485f9
velmyk <velmyk@gmail.com>
2016-06-06 19:56:08 +0300

    feature(es6-proxy) added some code examples with proxy

--- src/routes/es6/proxy/proxy.js
+++ src/routes/es6/proxy/proxy.js
@@ -7,5 +7,136 @@ module.exports = learnProxy;
 
 function learnProxy() {
 	templates.useContent(proxyTemplate);
-  	alert('proxy');
+	alert('Learn proxy');
+	proxySetGet();
+	proxyHas();
+	proxyDeleteProperty();
+	proxyEnumerate();
+	proxyApply();
+	proxyConstruct();
 };
+
+function proxySetGet() {
+	alert('Get/Set object');
+	console.log('<<<<<<<<<<<<');
+
+	let user = {};
+
+	let proxy = new Proxy(user, {
+		get(target, prop) {
+			console.log(`Reading ${prop}`);
+			return prop in target ? target[prop] : '!defined';
+			// return target[prop];
+		},
+		set(target, prop, value) {
+			console.log(`Writing ${prop} with ${value}`);
+			target[prop] = value;
+			return true;
+		}
+	});
+	proxy.firstName = 'Ben';
+	console.log(proxy.firstName);
+	console.log(proxy.LastName);
+}
+
+function proxyHas() {
+	alert('Has');
+	console.log('<<<<<<<<<<<<');
+
+	let dictionary = {
+  		'Hello': 'Привет'
+	};
+
+	dictionary = new Proxy(dictionary, {
+		has(target, phrase) {
+			return true;
+		}
+	});
+
+	console.log(`BlaBlaBla is not in dictionary but: ${'BlaBlaBla' in dictionary}`);
+}
+
+function proxyDeleteProperty() {
+	alert('DeleteProperty');
+	console.log('<<<<<<<<<<<<');
+
+	let dictionary = {
+		'Hello': 'Привет'
+	};
+
+	let proxy = new Proxy(dictionary, {
+		deleteProperty(target, phrase) {
+			return true;
+		}
+	});
+
+	delete proxy['Hello'];
+
+	console.log(`Hello not deleted: ${'Hello' in dictionary}`);
+	console.log('Hello' in proxy);
+}
+
+function proxyEnumerate() {
+	alert('Enumerate');
+	console.log('<<<<<<<<<<<<');
+
+	let obj = {
+		a: 1,
+		b: 1,
+		_c: 1,
+		_d: 1
+	};
+
+	let proxy = new Proxy(obj, {
+		enumerate(target) {
+			let props = Object.keys(target).filter(prop => prop[0] != '_');
+			return props[Symbol.iterator]();
+		}
+	});
+
+	console.log('enumerate is obsolete, so it may not work');
+
+	for(let prop in proxy) {
+		console.log(prop);
+	}
+}
+
+function proxyApply() {
+	alert('Apply');
+	console.log('<<<<<<<<<<<<');
+
+	function sum(a, b) {
+		return a + b;
+	}
+
+	let proxy = new Proxy(sum, {
+		apply: function(target, thisArg, argumentsList) {
+			console.log(`I'm proxy, I will calculate summ of: ${argumentsList}`);
+			return target.apply(thisArg, argumentsList);
+		}
+	});
+
+	console.log(`Sum: ${proxy(1, 2)} `);
+}
+
+function proxyConstruct() {
+	alert('Construct');
+	console.log('<<<<<<<<<<<<');
+
+	function User(name, surname) {
+		this.name = name;
+		this.surname = surname;
+	}
+
+	let UserProxy = new Proxy(User, {
+		construct: function(target, argumentsList) {
+			console.log(`Creating new User with args: ${argumentsList}`);
+			return new target(...argumentsList);
+		}
+	});
+
+	let user = new UserProxy("John", "Doe");
+
+	console.log(`New user's name: ${user.name} `);
+}
+
